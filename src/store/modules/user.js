83195d5f37ff8cffc1bd93d4ef91f5052d623896
suuxy <578981951@qq.com>
2023-03-20 16:37:36 +0800

    初始化token

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,13 +1,22 @@
+import { getToken, setToken, removeToken } from '@/utils/auth'
+const state = {
+  token: getToken()
+}
+// 修改state中的数据
+const mutations = {
+  setToken(state, token) {
+    state.token = token // 修改vuex中token值
+    setToken(token) // 将修改后的值同步到缓存
+  },
+  removeToken(state) {
+    state.token = null // vuex中的token值置空
+    removeToken() // 同步到缓存
+  }
+}
+const actions = {}
 export default {
   namespaced: true,
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
-
+  state,
+  mutations,
+  actions
 }
